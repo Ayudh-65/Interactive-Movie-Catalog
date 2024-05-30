01151870cb50766a2a04f9fb6908d5a9a7e0ff14
Ayudh Abhale <ayudhabhale65@gmail.com>
2024-05-30 15:28:10 +0530

    Add CORS config

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 import crud
 from schemas import Movie, MovieCreate
 from database import db
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
